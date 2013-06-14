d301074707d0e7319498b036a0f6b4916c0d4050
timgreenfield <tgreenfield@gmail.com>
2013-06-14 15:33:31 -0700

    WP7: Updating BCL dependency, porting WebVTT projects, and starting to port analytics projects

---
+++ Phone.WebVTT/Extensions/LinqExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Microsoft.WebVTT
+{
+    internal static class LinqExtensions
+    {
+        public static int RemoveAll<T>(this List<T> collection, Predicate<T> match)
+        {
+            int count = 0;
+            foreach (var item in collection.Where(match.Invoke).ToList())
+            {
+                collection.Remove(item);
+                count++;
+            }
+            return count;
+        }
+    }
+}
