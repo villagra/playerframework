2cf0596cf1ecb25a567bed2d71e6fe941d85e0ef
timgreenfield <tgreenfield@gmail.com>
2013-06-21 10:57:25 -0700

    support for running build script without changes to AnyCPU build config

--- Build/Deploy.Win8.Xaml.bat
+++ Build/Deploy.Win8.Xaml.bat
@@ -85,11 +85,11 @@ mkdir "Microsoft.PlayerFramework.Xaml.WebVTT\References\CommonConfiguration\neut
 
 cd..
 
-copy Win8.Xaml\bin\Release\Microsoft.PlayerFramework.dll  Build\Microsoft.PlayerFramework.Xaml\References\CommonConfiguration\neutral\
-copy Win8.Xaml\bin\Release\Microsoft.PlayerFramework.xml  Build\Microsoft.PlayerFramework.Xaml\References\CommonConfiguration\neutral\
-copy Win8.Xaml\bin\Release\Microsoft.PlayerFramework.pri  Build\Microsoft.PlayerFramework.Xaml\Redist\CommonConfiguration\neutral\Microsoft.PlayerFramework\
-copy Win8.Xaml\bin\Release\Themes\generic.xaml            Build\Microsoft.PlayerFramework.Xaml\Redist\CommonConfiguration\neutral\Microsoft.PlayerFramework\Themes
-copy Win8.Xaml\bin\Release\Themes\EntertainmentTheme.xaml Build\Microsoft.PlayerFramework.Xaml\Redist\CommonConfiguration\neutral\Microsoft.PlayerFramework\Themes
+copy Win8.Xaml\bin\x86\Release\Microsoft.PlayerFramework.dll  Build\Microsoft.PlayerFramework.Xaml\References\CommonConfiguration\neutral\
+copy Win8.Xaml\bin\x86\Release\Microsoft.PlayerFramework.xml  Build\Microsoft.PlayerFramework.Xaml\References\CommonConfiguration\neutral\
+copy Win8.Xaml\bin\x86\Release\Microsoft.PlayerFramework.pri  Build\Microsoft.PlayerFramework.Xaml\Redist\CommonConfiguration\neutral\Microsoft.PlayerFramework\
+copy Win8.Xaml\bin\x86\Release\Themes\generic.xaml            Build\Microsoft.PlayerFramework.Xaml\Redist\CommonConfiguration\neutral\Microsoft.PlayerFramework\Themes
+copy Win8.Xaml\bin\x86\Release\Themes\EntertainmentTheme.xaml Build\Microsoft.PlayerFramework.Xaml\Redist\CommonConfiguration\neutral\Microsoft.PlayerFramework\Themes
 
 copy Win8.Xaml.Adaptive\bin\x86\Release\Microsoft.PlayerFramework.Adaptive.pri					Build\Microsoft.PlayerFramework.Xaml.Adaptive\Redist\CommonConfiguration\neutral\Microsoft.PlayerFramework.Adaptive\
 copy Win8.Xaml.Adaptive\bin\x86\Release\Microsoft.AdaptiveStreaming.pri						    Build\Microsoft.PlayerFramework.Xaml.Adaptive\Redist\CommonConfiguration\neutral\Microsoft.AdaptiveStreaming\
@@ -109,33 +109,33 @@ copy Win8.Xaml.Adaptive\bin\ARM\Release\Microsoft.PlayerFramework.Adaptive.xml		
 copy Win8.Xaml.Adaptive\bin\ARM\Release\Microsoft.AdaptiveStreaming.winmd 					    Build\Microsoft.PlayerFramework.Xaml.Adaptive\References\CommonConfiguration\ARM\
 @rem copy Win8.Xaml.Adaptive\bin\ARM\Release\Microsoft.AdaptiveStreaming.xml				    Build\Microsoft.PlayerFramework.Xaml.Adaptive\References\CommonConfiguration\ARM\
 
-@rem copy Win8.Xaml.CaptionMarkers\bin\Release\Microsoft.PlayerFramework.CaptionMarkers.dll Build\Microsoft.PlayerFramework.Xaml.CaptionMarkers\References\CommonConfiguration\neutral\
-@rem copy Win8.Xaml.CaptionMarkers\bin\Release\Microsoft.PlayerFramework.CaptionMarkers.xml Build\Microsoft.PlayerFramework.Xaml.CaptionMarkers\References\CommonConfiguration\neutral\
-@rem copy Win8.Xaml.CaptionMarkers\bin\Release\Microsoft.PlayerFramework.CaptionMarkers.pri Build\Microsoft.PlayerFramework.Xaml.CaptionMarkers\Redist\CommonConfiguration\neutral\Microsoft.PlayerFramework.CaptionMarkers\
-@rem copy Win8.Xaml.CaptionMarkers\bin\Release\Themes\generic.xaml                          Build\Microsoft.PlayerFramework.Xaml.CaptionMarkers\Redist\CommonConfiguration\neutral\Microsoft.PlayerFramework.CaptionMarkers\themes\
+@rem copy Win8.Xaml.CaptionMarkers\bin\x86\Release\Microsoft.PlayerFramework.CaptionMarkers.dll Build\Microsoft.PlayerFramework.Xaml.CaptionMarkers\References\CommonConfiguration\neutral\
+@rem copy Win8.Xaml.CaptionMarkers\bin\x86\Release\Microsoft.PlayerFramework.CaptionMarkers.xml Build\Microsoft.PlayerFramework.Xaml.CaptionMarkers\References\CommonConfiguration\neutral\
+@rem copy Win8.Xaml.CaptionMarkers\bin\x86\Release\Microsoft.PlayerFramework.CaptionMarkers.pri Build\Microsoft.PlayerFramework.Xaml.CaptionMarkers\Redist\CommonConfiguration\neutral\Microsoft.PlayerFramework.CaptionMarkers\
+@rem copy Win8.Xaml.CaptionMarkers\bin\x86\Release\Themes\generic.xaml                          Build\Microsoft.PlayerFramework.Xaml.CaptionMarkers\Redist\CommonConfiguration\neutral\Microsoft.PlayerFramework.CaptionMarkers\themes\
 
-copy Win8.Xaml.TimedText\bin\Release\Microsoft.PlayerFramework.TimedText.dll Build\Microsoft.PlayerFramework.Xaml.TimedText\References\CommonConfiguration\neutral\
-copy Win8.Xaml.TimedText\bin\Release\Microsoft.PlayerFramework.TimedText.xml Build\Microsoft.PlayerFramework.Xaml.TimedText\References\CommonConfiguration\neutral\
-copy Win8.Xaml.TimedText\bin\Release\Microsoft.PlayerFramework.TimedText.pri Build\Microsoft.PlayerFramework.Xaml.TimedText\Redist\CommonConfiguration\neutral\Microsoft.PlayerFramework.TimedText\
-copy Win8.Xaml.TimedText\bin\Release\Microsoft.TimedText.dll				 Build\Microsoft.PlayerFramework.Xaml.TimedText\References\CommonConfiguration\neutral\
-@rem copy Win8.Xaml.TimedText\bin\Release\Microsoft.TimedText.xml			 Build\Microsoft.PlayerFramework.Xaml.TimedText\References\CommonConfiguration\neutral\
-copy Win8.Xaml.TimedText\bin\Release\Microsoft.TimedText.pri				 Build\Microsoft.PlayerFramework.Xaml.TimedText\Redist\CommonConfiguration\neutral\Microsoft.TimedText\
-copy Win8.TimedText\bin\Release\Themes\generic.xaml							 Build\Microsoft.PlayerFramework.Xaml.TimedText\Redist\CommonConfiguration\neutral\Microsoft.TimedText\themes\
+copy Win8.Xaml.TimedText\bin\x86\Release\Microsoft.PlayerFramework.TimedText.dll Build\Microsoft.PlayerFramework.Xaml.TimedText\References\CommonConfiguration\neutral\
+copy Win8.Xaml.TimedText\bin\x86\Release\Microsoft.PlayerFramework.TimedText.xml Build\Microsoft.PlayerFramework.Xaml.TimedText\References\CommonConfiguration\neutral\
+copy Win8.Xaml.TimedText\bin\x86\Release\Microsoft.PlayerFramework.TimedText.pri Build\Microsoft.PlayerFramework.Xaml.TimedText\Redist\CommonConfiguration\neutral\Microsoft.PlayerFramework.TimedText\
+copy Win8.Xaml.TimedText\bin\x86\Release\Microsoft.TimedText.dll				 Build\Microsoft.PlayerFramework.Xaml.TimedText\References\CommonConfiguration\neutral\
+@rem copy Win8.Xaml.TimedText\bin\x86\Release\Microsoft.TimedText.xml			 Build\Microsoft.PlayerFramework.Xaml.TimedText\References\CommonConfiguration\neutral\
+copy Win8.Xaml.TimedText\bin\x86\Release\Microsoft.TimedText.pri				 Build\Microsoft.PlayerFramework.Xaml.TimedText\Redist\CommonConfiguration\neutral\Microsoft.TimedText\
+copy Win8.TimedText\bin\x86\Release\Themes\generic.xaml							 Build\Microsoft.PlayerFramework.Xaml.TimedText\Redist\CommonConfiguration\neutral\Microsoft.TimedText\themes\
 
-copy Win8.Xaml.Advertising\bin\Release\Microsoft.PlayerFramework.Advertising.dll		Build\Microsoft.PlayerFramework.Xaml.Advertising\References\CommonConfiguration\neutral\
-copy Win8.Xaml.Advertising\bin\Release\Microsoft.PlayerFramework.Advertising.xml		Build\Microsoft.PlayerFramework.Xaml.Advertising\References\CommonConfiguration\neutral\
-copy Win8.Xaml.Advertising\bin\Release\Microsoft.PlayerFramework.Advertising.pri		Build\Microsoft.PlayerFramework.Xaml.Advertising\Redist\CommonConfiguration\neutral\Microsoft.PlayerFramework.Advertising\
-copy Win8.Xaml.Advertising\bin\Release\Themes\generic.xaml								Build\Microsoft.PlayerFramework.Xaml.Advertising\Redist\CommonConfiguration\neutral\Microsoft.PlayerFramework.Advertising\themes\
-copy Win8.Xaml.Advertising\bin\Release\Microsoft.VideoAdvertising.winmd 				Build\Microsoft.PlayerFramework.Xaml.Advertising\References\CommonConfiguration\neutral\
-@rem copy Win8.Xaml.Advertising\bin\Release\Microsoft.VideoAdvertising.xml				Build\Microsoft.PlayerFramework.Xaml.Advertising\References\CommonConfiguration\neutral\
-copy Win8.Xaml.Advertising\bin\Release\Microsoft.VideoAdvertising.pri					Build\Microsoft.PlayerFramework.Xaml.Advertising\Redist\CommonConfiguration\neutral\Microsoft.VideoAdvertising\
+copy Win8.Xaml.Advertising\bin\x86\Release\Microsoft.PlayerFramework.Advertising.dll		Build\Microsoft.PlayerFramework.Xaml.Advertising\References\CommonConfiguration\neutral\
+copy Win8.Xaml.Advertising\bin\x86\Release\Microsoft.PlayerFramework.Advertising.xml		Build\Microsoft.PlayerFramework.Xaml.Advertising\References\CommonConfiguration\neutral\
+copy Win8.Xaml.Advertising\bin\x86\Release\Microsoft.PlayerFramework.Advertising.pri		Build\Microsoft.PlayerFramework.Xaml.Advertising\Redist\CommonConfiguration\neutral\Microsoft.PlayerFramework.Advertising\
+copy Win8.Xaml.Advertising\bin\x86\Release\Themes\generic.xaml								Build\Microsoft.PlayerFramework.Xaml.Advertising\Redist\CommonConfiguration\neutral\Microsoft.PlayerFramework.Advertising\themes\
+copy Win8.Xaml.Advertising\bin\x86\Release\Microsoft.VideoAdvertising.winmd 				Build\Microsoft.PlayerFramework.Xaml.Advertising\References\CommonConfiguration\neutral\
+@rem copy Win8.Xaml.Advertising\bin\x86\Release\Microsoft.VideoAdvertising.xml				Build\Microsoft.PlayerFramework.Xaml.Advertising\References\CommonConfiguration\neutral\
+copy Win8.Xaml.Advertising\bin\x86\Release\Microsoft.VideoAdvertising.pri					Build\Microsoft.PlayerFramework.Xaml.Advertising\Redist\CommonConfiguration\neutral\Microsoft.VideoAdvertising\
 
-copy Win8.Xaml.Analytics\bin\Release\Microsoft.PlayerFramework.Analytics.dll	Build\Microsoft.PlayerFramework.Xaml.Analytics\References\CommonConfiguration\neutral\
-copy Win8.Xaml.Analytics\bin\Release\Microsoft.PlayerFramework.Analytics.xml	Build\Microsoft.PlayerFramework.Xaml.Analytics\References\CommonConfiguration\neutral\
-copy Win8.Xaml.Analytics\bin\Release\Microsoft.PlayerFramework.Analytics.pri	Build\Microsoft.PlayerFramework.Xaml.Analytics\Redist\CommonConfiguration\neutral\Microsoft.PlayerFramework.Analytics\
-copy Win8.VideoAnalytics\bin\Release\Microsoft.VideoAnalytics.winmd 			Build\Microsoft.PlayerFramework.Xaml.Analytics\References\CommonConfiguration\neutral\
-@rem copy Win8.VideoAnalytics\bin\Release\Microsoft.VideoAnalytics.xml			Build\Microsoft.PlayerFramework.Xaml.Analytics\References\CommonConfiguration\neutral\
-copy Win8.VideoAnalytics\bin\Release\Microsoft.VideoAnalytics.pri				Build\Microsoft.PlayerFramework.Xaml.Analytics\Redist\CommonConfiguration\neutral\Microsoft.VideoAnalytics\
+copy Win8.Xaml.Analytics\bin\x86\Release\Microsoft.PlayerFramework.Analytics.dll	Build\Microsoft.PlayerFramework.Xaml.Analytics\References\CommonConfiguration\neutral\
+copy Win8.Xaml.Analytics\bin\x86\Release\Microsoft.PlayerFramework.Analytics.xml	Build\Microsoft.PlayerFramework.Xaml.Analytics\References\CommonConfiguration\neutral\
+copy Win8.Xaml.Analytics\bin\x86\Release\Microsoft.PlayerFramework.Analytics.pri	Build\Microsoft.PlayerFramework.Xaml.Analytics\Redist\CommonConfiguration\neutral\Microsoft.PlayerFramework.Analytics\
+copy Win8.VideoAnalytics\bin\x86\Release\Microsoft.VideoAnalytics.winmd 			Build\Microsoft.PlayerFramework.Xaml.Analytics\References\CommonConfiguration\neutral\
+@rem copy Win8.VideoAnalytics\bin\x86\Release\Microsoft.VideoAnalytics.xml			Build\Microsoft.PlayerFramework.Xaml.Analytics\References\CommonConfiguration\neutral\
+copy Win8.VideoAnalytics\bin\x86\Release\Microsoft.VideoAnalytics.pri				Build\Microsoft.PlayerFramework.Xaml.Analytics\Redist\CommonConfiguration\neutral\Microsoft.VideoAnalytics\
 copy Lib\Portable\ZLib\bin\Release\ZLib.dll										Build\Microsoft.PlayerFramework.Xaml.Analytics\References\CommonConfiguration\neutral\
 
 copy Win8.AdaptiveStreaming.Analytics\bin\x86\Release\Microsoft.AdaptiveStreaming.Analytics.pri					Build\Microsoft.PlayerFramework.Xaml.Analytics\Redist\CommonConfiguration\neutral\Microsoft.AdaptiveStreaming.Analytics\
@@ -146,13 +146,13 @@ copy Win8.AdaptiveStreaming.Analytics\bin\x64\Release\Microsoft.AdaptiveStreamin
 copy Win8.AdaptiveStreaming.Analytics\bin\ARM\Release\Microsoft.AdaptiveStreaming.Analytics.winmd 				Build\Microsoft.PlayerFramework.Xaml.Analytics\References\CommonConfiguration\ARM\
 @rem copy Win8.AdaptiveStreaming.Analytics\bin\ARM\Release\Microsoft.AdaptiveStreaming.Analytics.xml			Build\Microsoft.PlayerFramework.Xaml.Analytics\References\CommonConfiguration\ARM\
 
-copy Win8.Xaml.WebVTT\bin\Release\Microsoft.PlayerFramework.WebVTT.dll		Build\Microsoft.PlayerFramework.Xaml.WebVTT\References\CommonConfiguration\neutral\
-@rem copy Win8.Xaml.WebVTT\bin\Release\Microsoft.PlayerFramework.WebVTT.xml	Build\Microsoft.PlayerFramework.Xaml.WebVTT\References\CommonConfiguration\neutral\
-copy Win8.Xaml.WebVTT\bin\Release\Microsoft.PlayerFramework.WebVTT.pri		Build\Microsoft.PlayerFramework.Xaml.WebVTT\Redist\CommonConfiguration\neutral\Microsoft.PlayerFramework.WebVTT\
-copy Win8.Xaml.WebVTT\bin\Release\Microsoft.WebVTT.winmd					Build\Microsoft.PlayerFramework.Xaml.WebVTT\References\CommonConfiguration\neutral\
-@rem copy Win8.Xaml.WebVTT\bin\Release\Microsoft.WebVTT.xml					Build\Microsoft.PlayerFramework.Xaml.WebVTT\References\CommonConfiguration\neutral\
-copy Win8.Xaml.WebVTT\bin\Release\Microsoft.WebVTT.pri						Build\Microsoft.PlayerFramework.Xaml.WebVTT\Redist\CommonConfiguration\neutral\Microsoft.WebVTT\
-copy Win8.WebVTT\bin\Release\Themes\generic.xaml							Build\Microsoft.PlayerFramework.Xaml.WebVTT\Redist\CommonConfiguration\neutral\Microsoft.WebVTT\themes\
+copy Win8.Xaml.WebVTT\bin\x86\Release\Microsoft.PlayerFramework.WebVTT.dll		Build\Microsoft.PlayerFramework.Xaml.WebVTT\References\CommonConfiguration\neutral\
+@rem copy Win8.Xaml.WebVTT\bin\x86\Release\Microsoft.PlayerFramework.WebVTT.xml	Build\Microsoft.PlayerFramework.Xaml.WebVTT\References\CommonConfiguration\neutral\
+copy Win8.Xaml.WebVTT\bin\x86\Release\Microsoft.PlayerFramework.WebVTT.pri		Build\Microsoft.PlayerFramework.Xaml.WebVTT\Redist\CommonConfiguration\neutral\Microsoft.PlayerFramework.WebVTT\
+copy Win8.Xaml.WebVTT\bin\x86\Release\Microsoft.WebVTT.winmd					Build\Microsoft.PlayerFramework.Xaml.WebVTT\References\CommonConfiguration\neutral\
+@rem copy Win8.Xaml.WebVTT\bin\x86\Release\Microsoft.WebVTT.xml					Build\Microsoft.PlayerFramework.Xaml.WebVTT\References\CommonConfiguration\neutral\
+copy Win8.Xaml.WebVTT\bin\x86\Release\Microsoft.WebVTT.pri						Build\Microsoft.PlayerFramework.Xaml.WebVTT\Redist\CommonConfiguration\neutral\Microsoft.WebVTT\
+copy Win8.WebVTT\bin\x86\Release\Themes\generic.xaml							Build\Microsoft.PlayerFramework.Xaml.WebVTT\Redist\CommonConfiguration\neutral\Microsoft.WebVTT\themes\
 
 @popd
 
