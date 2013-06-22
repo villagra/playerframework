158129c3a0f58c606bfb33619b7b2cf87f74f46d
timgreenfield <tgreenfield@gmail.com>
2013-06-22 16:25:57 -0700

    reverting changes to build script to use AnyCPU again

--- Build/Build.bat
+++ Build/Build.bat
@@ -5,12 +5,12 @@ cd..
 @set DEVENV=%ProgramFiles(x86)%\Microsoft Visual Studio 11.0\Common7\IDE\devenv.exe
 @IF NOT EXIST "%DEVENV%" SET DEVENV=%ProgramFiles(x86)%\Microsoft Visual Studio 11.0\Common7\IDE\VSWinExpress.exe
 
-@rem "%DEVENV%" /build "Release|AnyCPU" Microsoft.Win8.PlayerFramework.Js.sln
+"%DEVENV%" /build "Release|AnyCPU" Microsoft.Win8.PlayerFramework.Js.sln
 "%DEVENV%" /build "Release|x86"    Microsoft.Win8.PlayerFramework.Js.sln
 "%DEVENV%" /build "Release|x64"    Microsoft.Win8.PlayerFramework.Js.sln
 "%DEVENV%" /build "Release|ARM"    Microsoft.Win8.PlayerFramework.Js.sln
 
-@rem "%DEVENV%" /build "Release|AnyCPU" Microsoft.Win8.PlayerFramework.Xaml.sln
+"%DEVENV%" /build "Release|AnyCPU" Microsoft.Win8.PlayerFramework.Xaml.sln
 "%DEVENV%" /build "Release|x86"    Microsoft.Win8.PlayerFramework.Xaml.sln
 "%DEVENV%" /build "Release|x64"    Microsoft.Win8.PlayerFramework.Xaml.sln
 "%DEVENV%" /build "Release|ARM"    Microsoft.Win8.PlayerFramework.Xaml.sln
