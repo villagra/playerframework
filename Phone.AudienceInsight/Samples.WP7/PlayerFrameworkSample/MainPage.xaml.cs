fa8669ae05849d30adde08d2b70f6a6166b21b19
shaunmartin <shaun@vertigo.com>
2013-09-09 17:42:12 -0700

    Reorganizing WP7 and WP8 sample apps.

---
+++ Phone.AudienceInsight/Samples.WP7/PlayerFrameworkSample/MainPage.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+using Microsoft.PlayerFramework.Analytics;
+using Microsoft.PlayerFramework.Adaptive;
+using Microsoft.PlayerFramework.Adaptive.Analytics;
+using Microsoft.VideoAnalytics.VideoAdvertising;
+using Microsoft.PlayerFramework.Advertising;
+
+namespace PlayerFrameworkSample
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            player.Loaded += player_Loaded;
+        }
+
+        void player_Loaded(object sender, RoutedEventArgs e)
+        {
+            // Audience Insight config
+
+            var batchingConfig = Microsoft.AudienceInsight.BatchingConfigFactory.Load(new Uri("/PlayerFrameworkSample;component/AudienceInsightConfig.xml", UriKind.Relative));
+
+            var dataClient = (Microsoft.AudienceInsight.RESTDataClient)batchingConfig.BatchAgent;
+            dataClient.AdditionalHttpHeaders.Add("Authorization-Token", "{2842C782-562E-4250-A1A2-F66D55B5EA15}");
+
+            var batchinglogAgent = new Microsoft.AudienceInsight.BatchingLogAgent(batchingConfig);
+            var aiLoggingTarget = new Microsoft.VideoAnalytics.AudienceInsight.AudienceInsightLoggingTarget(batchinglogAgent);
+
+            Microsoft.VideoAnalytics.LoggingService.Current.LoggingTargets.Add(aiLoggingTarget);
+
+            // Player Framework analytics config
+
+            var analyticsConfig = Microsoft.VideoAnalytics.AnalyticsConfig.Load(new Uri("/PlayerFrameworkSample;component/AudienceInsightConfig.xml", UriKind.Relative));
+            var analyticsPlugin = new AnalyticsPlugin(analyticsConfig);
+
+            var adaptivePlugin = player.Plugins.OfType<AdaptivePlugin>().FirstOrDefault();
+
+            analyticsPlugin.AdaptiveMonitor = new AdaptiveMonitor(adaptivePlugin.SSME);
+            analyticsPlugin.EdgeServerMonitor = new Microsoft.VideoAnalytics.EdgeServerMonitor();
+
+            player.Plugins.Add(analyticsPlugin);
+
+            // Audience Insight ad tracking config
+
+            AdvertisingLoggingSource.Initialize(player.GetAdHandlerPlugin().AdHandlers, analyticsPlugin.AnalyticsCollector);
+        }
+    }
+}
---
+++ Phone.AudienceInsight/Samples.WP8/StandaloneSample/MainPage.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using StandaloneSample.Resources;
+using Microsoft.AudienceInsight;
+
+namespace StandaloneSample
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        BatchingLogAgent batchingLogAgent;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            var batchingConfig = BatchingConfigFactory.Load(new Uri("/StandaloneSample;component/AudienceInsightConfig.xml", UriKind.Relative));
+
+            var dataClient = (RESTDataClient)batchingConfig.BatchAgent;
+            dataClient.AdditionalHttpHeaders.Add("Authorization-Token", "{2842C782-562E-4250-A1A2-F66D55B5EA15}");
+
+            batchingLogAgent = new Microsoft.AudienceInsight.BatchingLogAgent(batchingConfig);
+
+            base.OnNavigatedTo(e);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var customLog = new CustomLog()
+            {
+                CustomProperty = "testing",
+                CustomPropertyNumber = 3.14159,
+                CustomPropertyBool = true
+            };
+
+            batchingLogAgent.LogEntry(customLog);
+        }
+    }
+
+    public class CustomLog : ILog
+    {
+        public CustomLog()
+        {
+            Type = "CustomLog";
+            Id = Guid.NewGuid();
+            ExtraData = new Dictionary<string, object>();
+            TimeStamp = DateTimeOffset.Now;
+        }
+
+        public string CustomProperty { get; set; }
+        public double CustomPropertyNumber { get; set; }
+        public bool CustomPropertyBool { get; set; }
+
+        public IDictionary<string, object> ExtraData { get; private set; }
+
+        public IDictionary<string, object> GetData()
+        {
+            var data = this.CreateBasicLogData();
+            data.Add("CustomProperty", CustomProperty);
+            data.Add("CustomPropertyNumber", CustomPropertyNumber);
+            data.Add("CustomPropertyBool", CustomPropertyBool);
+            return data;
+        }
+
+        public Guid Id { get; private set; }
+        public DateTimeOffset TimeStamp { get; set; }
+        public string Type { get; private set; }
+    }
+}
