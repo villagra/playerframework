9433c3f4bf8c836a642b709e923b7f95e5a34864
shaunmartin <shaun@vertigo.com>
2013-09-19 10:29:43 -0700

    Updating config file parameter name.

--- Win8.AudienceInsight/BatchAgentFactory.cs
+++ Win8.AudienceInsight/BatchAgentFactory.cs
@@ -53,7 +53,7 @@ namespace Microsoft.AudienceInsight
                         case "version":
                             serviceVersion = reader.ReadElementContentAsInt();
                             break;
-                        case "compressed":
+                        case "compression":
                             compress = Convert.ToBoolean(reader.ReadElementContentAsInt());
                             break;
                         case "serializationFormat":
