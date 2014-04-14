839ac5811ac699300446d0511686a77d8f797a97
timgreenfield <tgreenfield@gmail.com>
2014-04-14 14:37:21 -0700

    WinJS: Adding support for pf-compact and pf-portrait CSS on control panel when control panel resizes

--- Universal.Js.Core/js/plugins/ControlPlugin.js
+++ Universal.Js.Core/js/plugins/ControlPlugin.js
@@ -13,6 +13,9 @@
         this._controlContainerElement = null;
         this._controlPanelElement = null;
         this._flyoutContainerElement = null;
+        this._compactThresholdInInches = 5.0;
+        this._orientation = "landscape";
+        this._isCompact = false;
 
         PlayerFramework.PluginBase.call(this, options);
     }, {
@@ -25,16 +28,59 @@
             this.mediaPlayer.removeClass("pf-show-control-container");
         },
 
+        compactThresholdInInches: {
+            get: function () {
+                return this._compactThresholdInInches;
+            },
+            set: function (value) {
+                this._compactThresholdInInches = value;
+            }
+        },
+
+        isCompact: {
+            get: function () {
+                return this._isCompact;
+            },
+            set: function (value) {
+                if (value !== this._isCompact) {
+                    if (this._isCompact) {
+                        WinJS.Utilities.removeClass(this._controlPanelElement, "pf-compact");
+                    }
+                    this._isCompact = value;
+                    if (this._isCompact) {
+                        WinJS.Utilities.addClass(this._controlPanelElement, "pf-compact");
+                    }
+                }
+            }
+        },
+
+        orientation: {
+            get: function () {
+                return this._orientation;
+            },
+            set: function (value) {
+                if (value !== this._orientation) {
+                    if (this._orientation === "portrait") {
+                        WinJS.Utilities.removeClass(this._controlPanelElement, "pf-portrait");
+                    }
+                    this._orientation = value;
+                    if (this._orientation === "portrait") {
+                        WinJS.Utilities.addClass(this._controlPanelElement, "pf-portrait");
+                    }
+                }
+            }
+        },
+
         // Private Methods
         _setElement: function () {
             this._controlContainerElement = PlayerFramework.Utilities.createElement(this.mediaPlayer.element, ["div", { "class": "pf-control-container" }]);
             this._controlPanelElement = PlayerFramework.Utilities.createElement(this._controlContainerElement, ["div", { "data-win-control": "PlayerFramework.UI.ControlPanel", "data-win-bind": "winControl.isPlayPauseHidden: isPlayPauseHidden; winControl.isPlayResumeHidden: isPlayResumeHidden; winControl.isPauseHidden: isPauseHidden; winControl.isReplayHidden: isReplayHidden; winControl.isRewindHidden: isRewindHidden; winControl.isFastForwardHidden: isFastForwardHidden; winControl.isSlowMotionHidden: isSlowMotionHidden; winControl.isSkipPreviousHidden: isSkipPreviousHidden; winControl.isSkipNextHidden: isSkipNextHidden; winControl.isSkipBackHidden: isSkipBackHidden; winControl.isSkipAheadHidden: isSkipAheadHidden;  winControl.isElapsedTimeHidden: isElapsedTimeHidden; winControl.isRemainingTimeHidden: isRemainingTimeHidden; winControl.isTotalTimeHidden: isTotalTimeHidden; winControl.isTimelineHidden: isTimelineHidden; winControl.isGoLiveHidden: isGoLiveHidden; winControl.isCaptionsHidden: isCaptionsHidden; winControl.isAudioHidden: isAudioHidden; winControl.isVolumeMuteHidden: isVolumeMuteHidden; winControl.isVolumeHidden: isVolumeHidden; winControl.isMuteHidden: isMuteHidden; winControl.isFullScreenHidden: isFullScreenHidden; winControl.isStopHidden: isStopHidden; winControl.isInfoHidden: isInfoHidden; winControl.isMoreHidden: isMoreHidden; winControl.isZoomHidden: isZoomHidden; winControl.isSignalStrengthHidden: isSignalStrengthHidden; winControl.isMediaQualityHidden: isMediaQualityHidden;" }]);
-            
+
             this._controlContainerElement.winControl = this;
 
             WinJS.UI.processAll(this._controlContainerElement);
             PlayerFramework.Binding.processAll(this._controlContainerElement, this.mediaPlayer.interactiveViewModel);
-            
+
             if (!WinJS.Utilities.isPhone) {
                 this._flyoutContainerElement = PlayerFramework.Utilities.createElement(document.body, ["div", { "class": "pf-flyout-container" }]);
                 this._controlPanelElement.winControl.flyoutContainerElement = this._flyoutContainerElement;
@@ -48,6 +94,13 @@
             this._bindEvent("interactiveviewmodelchange", this.mediaPlayer, this._onMediaPlayerInteractiveViewModelChange);
             this._bindEvent("interactivestatechange", this.mediaPlayer, this._onMediaPlayerInteractiveStateChange);
             this._bindEvent("transitionend", this._controlPanelElement, this._onControlPanelTransitionEnd);
+            if (PlayerFramework.Utilities.isWinJS1) {
+                this._bindEvent("resize", this.mediaPlayer.element, this._onMediaPlayerResize);
+            }
+            else { // IE11 no longer supports resize event for arbitrary elements. The best we can do is listen to the window resize event.
+                this._bindEvent("resize", window, this._onMediaPlayerResize);
+            }
+            this._onMediaPlayerResize();
 
             if (this.mediaPlayer.isInteractive) {
                 this.show();
@@ -65,6 +118,12 @@
             this._unbindEvent("interactiveviewmodelchange", this.mediaPlayer, this._onMediaPlayerInteractiveViewModelChange);
             this._unbindEvent("interactivestatechange", this.mediaPlayer, this._onMediaPlayerInteractiveStateChange);
             this._unbindEvent("transitionend", this._controlPanelElement, this._onControlPanelTransitionEnd);
+            if (PlayerFramework.Utilities.isWinJS1) {
+                this._unbindEvent("resize", this.mediaPlayer.element, this._onMediaPlayerResize);
+            }
+            else {
+                this._unbindEvent("resize", window, this._onMediaPlayerResize);
+            }
 
             PlayerFramework.Utilities.removeElement(this._controlContainerElement);
             if (this._flyoutContainerElement) {
@@ -93,7 +152,30 @@
             if (e.target === this._controlPanelElement && this._controlPanelElement.winControl.hidden) {
                 this.hide();
             }
+        },
+
+        _onMediaPlayerResize: function () {
+            var w = this.mediaPlayer.element.scrollWidth;
+            var h = this.mediaPlayer.element.scrollHeight;
+
+            this.orientation = (h > w) ? "portrait" : "landscape";
+            if (PlayerFramework.Utilities.isWinJS1) {
+                this.isCompact = Windows.UI.ViewManagement.ApplicationView.value === Windows.UI.ViewManagement.ApplicationViewState.snapped;
+            }
+            else {
+                var physicalSize = this._getPhysicalSize({ width: w, height: h });
+                this.isCompact = (physicalSize.width <= this.compactThresholdInInches);
+            }
+        },
+
+        _getPhysicalSize: function (size) {
+            var displayInfo = Windows.Graphics.Display.DisplayInformation.getForCurrentView();
+            var scale = displayInfo.resolutionScale / 100;
+            var w = size.width * scale / displayInfo.rawDpiX;
+            var h = size.height * scale / displayInfo.rawDpiY;
+            return { width: w, height: h };
         }
+
     }, {
         // Static Properties
         isDeclarativeControlContainer: {
