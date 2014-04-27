fca8e757b1fa7cfeb5ba5d36138867ddb7d38f95
timgreenfield <tgreenfield@gmail.com>
2014-04-27 15:57:38 -0700

    Adding WP81 SL test project

---
+++ WP81.SL.Test/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.PlayerFramework.WP81.Test.Resources;
+
+namespace Microsoft.PlayerFramework.WP81.Test
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
---
+++ WP81.SL.Test/MainPage.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using Microsoft.PlayerFramework.WP81.Test.Resources;
+using Microsoft.PlayerFramework.Adaptive;
+
+namespace Microsoft.PlayerFramework.WP81.Test
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        AdaptivePlugin adaptivePlugin;
+        MediaState deactivatedState;
+        MediaState playerState;
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            // add adaptive plugin in order to play smooth streaming
+            adaptivePlugin = new Microsoft.PlayerFramework.Adaptive.AdaptivePlugin();
+            player.Plugins.Add(adaptivePlugin);
+            player.VisualMarkers.Add(new VisualMarker() { Time = TimeSpan.FromSeconds(10) });
+            player.VisualMarkers.Add(new VisualMarker() { Time = TimeSpan.FromSeconds(20) });
+
+            PhoneApplicationService.Current.Deactivated += Current_Deactivated;
+            PhoneApplicationService.Current.Activated += Current_Activated;
+        }
+
+        protected override void OnNavigatingFrom(NavigatingCancelEventArgs e)
+        {
+            playerState = player.GetMediaState();
+            base.OnNavigatingFrom(e);
+        }
+
+        void Current_Activated(object sender, ActivatedEventArgs e)
+        {
+            if (deactivatedState != null)
+            {
+                player.RestoreMediaState(deactivatedState);
+            }
+        }
+
+        void Current_Deactivated(object sender, DeactivatedEventArgs e)
+        {
+            player.Close(); // shut things like ads down.
+            deactivatedState = playerState;
+        }
+    }
+}
