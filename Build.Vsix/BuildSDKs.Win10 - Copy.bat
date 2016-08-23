6725b3683e163fa631902097bee9a39bb0a4b07d
villagra <villagra@gmail.com>
2016-08-23 13:34:47 +0200

    Build processs (win10 only)

--- Build.Vsix/BuildSDKs.Win10 - Copy.bat
+++ Build.Vsix/BuildSDKs.Win10 - Copy.bat
@@ -3,8 +3,14 @@
 call Cleanup.bat
 call Build.Win10.Xaml.bat
 call Build.Win10.JS.bat
+call Build.Win81.Xaml.bat
+call Build.Win81.JS.bat
+call Build.WP81.Xaml.bat
+call Build.WP81.JS.bat
 
 call Package.Win10.bat
+call Package.Win81.bat
+call Package.WP81.bat
 
 copy Win10.extension.vsixmanifest Microsoft.PlayerFramework\extension.vsixmanifest
 
